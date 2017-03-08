bfe048cc969a0a64a2c1c89ff29e533c96c2f19f
Bertrand PISELLI <bertrand.piselli@smartplace.pro>
2017-03-08 16:01:24 +0100

    CARS allow ALL

--- core/gwapit/settings.py
+++ core/gwapit/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'gwapit.quickstart'
 ]
 
@@ -48,7 +49,9 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'gwapit.urls'
@@ -120,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+## CORS
+
+CORS_ORIGIN_ALLOW_ALL = True
+
